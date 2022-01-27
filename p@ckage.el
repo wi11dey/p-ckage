255183048f47a6de14e11b446a4a8c91f35ebb3c
Will Dey <52264243+wi11dey@users.noreply.github.com>
2022-01-27 19:10:37 +0000

    Update and rename p@ck.el to p@ckage.el

--- p@ckage.el
+++ p@ckage.el
@@ -1,4 +1,4 @@
-;;; p@ck.el --- Package configuration language -*- lexical-binding: t -*-
+;;; p@ckage.el --- Package configuration language -*- lexical-binding: t -*-
 
 ;; Author: Will Dey
 ;; Maintainer: Will Dey
@@ -24,48 +24,45 @@
 ;; Generate README:
 ;;; Commentary:
 
-;; Ah p@ck.
-
-;; commentary
-;; Reader constructs
+;; //Reader macros for your packages.//
 
 ;;; Code:
 
 (require 'macroexp)
 
-;; TODO Timing functions (that don't need p@ck during runtime, only to view the % report)
+;; TODO Timing functions (that don't need p@ckage during runtime, only to view the % report)
 ;; TODO better detection of what is a function for @functions
 
-(defgroup p@ck nil
+(defgroup p@ckage nil
   ""
   :group 'startup)
 
-(defcustom p@ck-enable-timing t
+(defcustom p@ckage-enable-timing t
   ""
   :type 'boolean)
 
-(defun p@ck--get-body (name autoloads-ptr body)
+(defun p@ckage--get-body (name autoloads-ptr body)
   (let ((next (cadr body)))
     (unless (consp next)
       (user-error "Expected ( after %s" (car body)))
     (setf (cdr body) (cddr body))
     (if (listp (car next))
-	(p@ck--process-body name autoloads-ptr next)
-      (p@ck--process-body name autoloads-ptr (cdr next))
+	(p@ckage--process-body name autoloads-ptr next)
+      (p@ckage--process-body name autoloads-ptr (cdr next))
       (list next))))
 
-(defun p@ck--add-autoload (autoloads-ptr func)
+(defun p@ckage--add-autoload (autoloads-ptr func)
   (unless (memq func (car autoloads-ptr))
     (push func (car autoloads-ptr))))
 
 ;;;; @ symbols
-(defun p@ck--autoload-symbol (autoloads-ptr symbol)
+(defun p@ckage--autoload-symbol (autoloads-ptr symbol)
   (when (and (symbolp symbol)
 	     (setq symbol (symbol-name symbol))
 	     (> (length symbol) 1)
 	     (eq (aref symbol 0) ?@))
     (setq symbol (intern (substring symbol 1)))
-    (p@ck--add-autoload autoloads-ptr symbol)
+    (p@ckage--add-autoload autoloads-ptr symbol)
     symbol))
 
 ;; ~ eval-when-compile
@@ -74,34 +71,34 @@
 ;; ^ require
 ;; @' autoload quote
 ;; $ [package-name]
-(defun p@ck--process-body (name autoloads-ptr body)
+(defun p@ckage--process-body (name autoloads-ptr body)
   (let ((current body))
     (while (consp current)
       (pcase (car current)
 	;;;; ~
 	('~
 	 ;; `eval-when-compile'
-	 (let ((prog (macroexp-progn (p@ck--get-body name autoloads-ptr current))))
+	 (let ((prog (macroexp-progn (p@ckage--get-body name autoloads-ptr current))))
 	   (setf (car current) `',(if (bound-and-true-p byte-compile-current-buffer)
 				      (byte-compile-eval prog)
 				    (eval prog lexical-binding)))))
 	;;;; !
 	('!
 	 ;; `eval-and-compile'
-	 (let ((prog (macroexp-progn (p@ck--get-body name autoloads-ptr current))))
+	 (let ((prog (macroexp-progn (p@ckage--get-body name autoloads-ptr current))))
 	   (eval prog lexical-binding)
 	   (setf (car current) prog)))
 	;;;; _
 	('_
 	 ;; `eval-after-load'
 	 (setf (car current) `(with-eval-after-load ',name
-				,@(p@ck--get-body name autoloads-ptr current))))
+				,@(p@ckage--get-body name autoloads-ptr current))))
 	;;;; @ quote
 	('@
 	 (let ((next (cadr current)))
 	   (unless (eq (car-safe next) 'quote)
 	     (user-error "Expected ' after @"))
-	   (p@ck--add-autoload autoloads-ptr (cadr next))
+	   (p@ckage--add-autoload autoloads-ptr (cadr next))
 	   (setf (car next) 'function
 		 (car current) next
 		 (cdr current) (cddr current))))
@@ -121,19 +118,19 @@
 	 (setf (car current) `(require ',name)))
 	;;;; @ functions
 	((pred consp)
-	 (unless (memq (caar current) '(quote function p@ck))
-	   (let ((symbol (p@ck--autoload-symbol autoloads-ptr (caar current))))
+	 (unless (memq (caar current) '(quote function p@ckage))
+	   (let ((symbol (p@ckage--autoload-symbol autoloads-ptr (caar current))))
 	     (when symbol
 	       (setf (caar current) symbol)))
-	   (setf (cdar current) (p@ck--process-body name autoloads-ptr (cdar current))))))
+	   (setf (cdar current) (p@ckage--process-body name autoloads-ptr (cdar current))))))
       (setq current (cdr current))))
   body)
 
-(defun p@ck--replace (name-string body &optional half)
+(defun p@ckage--replace (name-string body &optional half)
   (cond (half
 	 (let ((value (funcall half body)))
 	   (cond ((consp value)
-		  (p@ck--replace name-string value))
+		  (p@ckage--replace name-string value))
 		 ((symbolp value)
 		  (let* ((symbol (symbol-name value))
 			 (replacement (save-match-data
@@ -149,42 +146,42 @@
 				 #'setcdr)
 			       body
 			       replacement)))))))
-	((eq (car body) 'p@ck)
-	 (p@ck--replace name-string (cdr body) #'car))
+	((eq (car body) 'p@ckage)
+	 (p@ckage--replace name-string (cdr body) #'car))
 	(t
-	 (p@ck--replace name-string body #'car)
-	 (p@ck--replace name-string body #'cdr))))
+	 (p@ckage--replace name-string body #'car)
+	 (p@ckage--replace name-string body #'cdr))))
 
-(defun p@ck--top-level-autoloads (autoloads-ptr body)
+(defun p@ckage--top-level-autoloads (autoloads-ptr body)
   (while body
-    (if (p@ck--autoload-symbol autoloads-ptr (car body))
+    (if (p@ckage--autoload-symbol autoloads-ptr (car body))
 	(setf (car body) (cadr body)
 	      (cdr body) (cddr body))
       (setq body (cdr body)))))
 
-(defvar p@ck-timing nil
+(defvar p@ckage-timing nil
   "")
 
 ;;;###autoload
-(defun p@ck-report ()
-  "from `p@ck-timing'"
+(defun p@ckage-report ()
+  "from `p@ckage-timing'"
   (interactive)
-  (if (bound-and-true-p p@ck-timing)
+  (if (bound-and-true-p p@ckage-timing)
       (progn
 	)
-    (message "p@ck-report: No timing information available")))
+    (message "p@ckage-report: No timing information available")))
 
 ;;;###autoload
-(defmacro p@ck (name &rest body)
+(defmacro p@ckage (name &rest body)
   ""
   (declare (indent 1))
   (let ((name-string (symbol-name name))
 	(autoloads-ptr (list nil)))
     (condition-case err
 	(progn
-          (p@ck--replace name-string body)
-	  (p@ck--top-level-autoloads autoloads-ptr body)
-	  (p@ck--process-body name autoloads-ptr body)
+          (p@ckage--replace name-string body)
+	  (p@ckage--top-level-autoloads autoloads-ptr body)
+	  (p@ckage--process-body name autoloads-ptr body)
 	  (dolist (func (car autoloads-ptr))
 	    ;; Effectively `eval-and-compile'.
 	    (autoload func name-string)
@@ -199,27 +196,27 @@
 				 ((pred keymapp) ''keymap)
 				 ((pred macrop)  ''macro)))))
 		  body))
-	  (when p@ck-enable-timing
-	    (setq p@ck-timing (assq-delete-all name p@ck-timing)))
+	  (when p@ckage-enable-timing
+	    (setq p@ckage-timing (assq-delete-all name p@ckage-timing)))
 	  `(condition-case-unless-debug err
 	       (progn
 		 ;; `add-hook'
-		 ,@(when p@ck-enable-timing
-		     `((add-hook 'p@ck-timing (list ',name :start (current-time)))))
+		 ,@(when p@ckage-enable-timing
+		     `((add-hook 'p@ckage-timing (list ',name :start (current-time)))))
 		 ,@body
-		 ,@(when p@ck-enable-timing
-		     `((add-hook 'p@ck-timing (list ',name :end (current-time))))))
-	     (error (display-warning 'p@ck
+		 ,@(when p@ckage-enable-timing
+		     `((add-hook 'p@ckage-timing (list ',name :end (current-time))))))
+	     (error (display-warning 'p@ckage
 				     (format "%s: %s"
 					     ,name-string
 					     (error-message-string err))
 				     :error))))
-      (error (display-warning 'p@ck
+      (error (display-warning 'p@ckage
 			      (format "%s compilation: %s"
 				      name-string
 				      (error-message-string err))
 			      :error)))))
 
-(provide 'p@ck)
+(provide 'p@ckage)
 
-;;; p@ck.el ends here
+;;; p@ckage.el ends here
